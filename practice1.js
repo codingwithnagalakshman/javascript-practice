56fe4fbdad34a34149032fd8b65710d75cd41028
Nagalakshman Chirukuri <codingwithnagalakshman@gmail.com>
2022-02-06 21:45:50 +0000

    Javascript basics practice

---
+++ practice1.js
@@ -0,0 +1,27 @@
+const obj = {
+    a: "One",
+    b: "Two",
+    a: "Three"
+}
+
+console.log(obj);
+
+console.log(1 < 2 < 3);
+
+console.log( 1 > 2 > 3);
+
+console.log("a" < "b" < "c");
+
+console.log("a" > "b" > "c");
+
+console.log(3 < 2 < 1);
+
+console.log(3 < 2);
+
+console.log(false < 1);
+
+console.log(false > 1);
+
+console.log(true < 1);
+
+console.log(true > 1);
